99bdbfefbd54d602a56ebafd02bd10928e369b7b
zayyid123 <mochamadzayyid@gmail.com>
2023-10-09 09:50:13 +0700

    add navbar

---
+++ src/app/components/NavbarComponenet.js
@@ -0,0 +1,47 @@
+import Image from 'next/image'
+import React from 'react'
+
+const NavbarComponenet = ({ isOpen = true, setisOpen }) => {
+
+  return (
+    <nav className="bg-white fixed w-full z-20 top-0 left-0 border-b border-gray-200">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        {/* logo */}
+        <a href="https://flowbite.com/" className="flex items-center">
+          <Image src="https://flowbite.com/docs/images/logo.svg" width={60} height={60} className="h-10 mr-3" alt="Flowbite Logo" />
+          <span className="self-center text-2xl font-semibold whitespace-nowrap">Flowbite</span>
+        </a>
+
+        {/* icon hamburger menu */}
+        <div className="flex md:hidden md:order-2">
+          <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-sticky" aria-expanded="false">
+            <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
+            </svg>
+          </button>
+        </div>
+
+        {/* menu */}
+        <div className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${!isOpen && 'hidden'}`} id="navbar-sticky">
+          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white">
+            <li>
+              <a href="#" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0" aria-current="page">Home</a>
+            </li>
+            <li>
+              <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0">About</a>
+            </li>
+            <li>
+              <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0">Services</a>
+            </li>
+            <li>
+              <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0">Contact</a>
+            </li>
+          </ul>
+        </div>
+        
+      </div>
+    </nav>
+  )
+}
+
+export default NavbarComponenet

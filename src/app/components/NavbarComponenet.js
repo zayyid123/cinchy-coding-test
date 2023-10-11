f058073bb7de1ab88b4134db0754f7aadbe60944
zayyid123 <mochamadzayyid@gmail.com>
2023-10-11 13:24:59 +0700

    add some progress

--- src/app/components/NavbarComponenet.js
+++ src/app/components/NavbarComponenet.js
@@ -1,44 +1,68 @@
+"use client"
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 
-const NavbarComponenet = ({ isOpen = true, setisOpen }) => {
+// image
+import Logo from '../assets/image/logo.png'
+
+const NavbarComponenet = () => {
+  const [isOpen, setisOpen] = useState(false);
+
+  console.log(isOpen)
 
   return (
     <nav className="bg-white fixed w-full z-20 top-0 left-0 border-b border-gray-200">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         {/* logo */}
         <a href="https://flowbite.com/" className="flex items-center">
-          <Image src="https://flowbite.com/docs/images/logo.svg" width={60} height={60} className="h-10 mr-3" alt="Flowbite Logo" />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap">Flowbite</span>
+          <Image src={Logo} className="w-[278px] mr-3 max-[355px]:w-[220px]" alt="Flowbite Logo" />
         </a>
 
         {/* icon hamburger menu */}
-        <div className="flex md:hidden md:order-2">
-          <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-sticky" aria-expanded="false">
-            <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+        <div 
+          className="flex md:hidden"
+          onClick={() => {
+            setisOpen(!isOpen)
+          }}
+        >
+          <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 my-4 w-[33px] justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-sticky" aria-expanded="false">
+            <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
               <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
             </svg>
           </button>
         </div>
 
         {/* menu */}
-        <div className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${!isOpen && 'hidden'}`} id="navbar-sticky">
-          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white">
-            <li>
-              <a href="#" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0" aria-current="page">Home</a>
-            </li>
-            <li>
-              <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0">About</a>
-            </li>
-            <li>
-              <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0">Services</a>
-            </li>
-            <li>
-              <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0">Contact</a>
-            </li>
-          </ul>
+        <div className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${!isOpen && 'hidden'} nunito-sans font-normal`} id="navbar-sticky">
+          <div className="flex flex-col p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-slate-200 md:flex-row md:mt-0 md:border-0 md:bg-white">
+            
+            {/* about us */}
+            <div className='cursor-pointer p-4'>
+              <p>About Us</p>
+            </div>
+
+            {/* pricing */}
+            <div className='cursor-pointer p-4'>
+              <p>Pricing</p>
+            </div>
+
+            {/* FAQ */}
+            <div className='cursor-pointer p-4'>
+              <p>FAQ</p>
+            </div>
+
+            {/* My Booking */}
+            <div className='cursor-pointer p-4'>
+              <p>My Booking</p>
+            </div>
+
+            {/* Login */}
+            <div className='cursor-pointer flex justify-center items-center px-4 my-2 rounded-[4px] border mx-3 md:mx-0 py-3 md:py-0 border-[#00332C]'>
+              <p>Login</p>
+            </div>
+          </div>
         </div>
-        
+
       </div>
     </nav>
   )
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,33 @@
 import Image from 'next/image'
 
+// image
+import BgImage from './assets/image/bg.png'
+import Avatar from './assets/image/avatar.png'
+
 export default function Home() {
   return (
-    <main className="flex h-[3000px] flex-col items-center justify-between p-24">
-      <div className='text-red-300'>hello</div>
+    <main className="flex justify-start items-center flex-col">
+      {/* image */}
+      <div
+        className='mt-[89px] relative bg-red-100'
+      >
+        {/* bg image */}
+        <Image src={BgImage} className='object-cover h-[500px]' alt='cichy.life bg'/>
+        
+        <div className='flex justify-evenly items-center absolute bottom-[35%] md:bottom-0 w-[100%]'>
+          {/* descript */}
+          <div className='text-white m-3'>
+            <div className='text-5xl font-semibold outfit mb-4'>Get Started</div>
+
+            <div className='max-w-[401px] nunito-sans text-lg'>
+              {`Flight - confirmed! Hotel - sorted! And now, it's time to embark on your Bali Scooter Rental adventure! If you're still uncertain about your next steps, don’t worry, as we're here to address all your queries and concerns.`}
+            </div>
+          </div>
+
+          {/* avatar */}
+          <Image src={Avatar} className='w-[478px] object-cover hidden md:block' alt='cichy.life avatar'/>
+        </div>
+      </div>
     </main>
   )
 }
